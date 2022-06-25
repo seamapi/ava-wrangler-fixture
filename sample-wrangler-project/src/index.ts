cfe35ac976b70cfae8f337db25d957634e43652c
seveibar <seveibar@gmail.com>
2022-06-24 19:23:06 -0700

    sample wrangler project working

--- sample-wrangler-project/src/index.ts
+++ sample-wrangler-project/src/index.ts
@@ -1,2 +1,19 @@
 export default {
+  async fetch(
+    req: Request,
+    env: any,
+    ctx: ExecutionContext
+  ): Promise<Response> {
+    const returnPayload = {
+      ok: true,
+      body: await req.json().catch((e) => e.toString()),
+    }
+
+    return new Response(JSON.stringify(returnPayload, null, "  "), {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+  },
 }
