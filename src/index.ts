bea645e922a222c869c416f443ea6841b09db228
seveibar <seveibar@gmail.com>
2022-06-24 19:31:32 -0700

    import start of wrangler fixture

---
+++ src/index.ts
@@ -0,0 +1,65 @@
+/**
+ * Unforunately, as of 2022-06-21 wrangler/miniflare doesn't support an easy
+ * way for me to create a server programmatically, so we'll need to call out
+ * to shell
+ */
+
+import child_process from "child_process"
+import chalk from "chalk"
+import getPort from "get-port"
+import defaultAxios from "axios"
+
+export const getTestServer = async (t: any) => {
+  // axiosMonkeypatch()
+  const port = await getPort()
+
+  const proc = child_process.spawn(
+    `npx`,
+    ["wrangler", "dev", "-l", "--port", port, "./src/index.ts"],
+    {
+      shell: true,
+    }
+  )
+
+  let isClosed = false,
+    hasStarted = false
+  proc.stdout.on("data", (data: any) => {
+    console.log(chalk.yellow(data.toString()))
+    if (data.toString().includes("Listening on localhost")) {
+      console.log("has started")
+      hasStarted = true
+    }
+  })
+
+  proc.stderr.on("data", (data: any) => {
+    console.log(chalk.red(data.toString()))
+  })
+
+  proc.on("close", (code: any) => {
+    console.log({ close: true })
+    isClosed = true
+  })
+
+  t.teardown(() => {
+    if (!isClosed) {
+      proc.kill("SIGINT")
+    }
+  })
+
+  const serverUrl = `http://localhost:${port}`
+
+  const axios = defaultAxios.create({
+    baseURL: serverUrl,
+  })
+
+  while (!hasStarted) {
+    await new Promise((resolve) => setTimeout(resolve, 20))
+  }
+
+  return {
+    serverUrl,
+    axios,
+  }
+}
+
+export default getTestServer
